11ed56f270e93a40467591f5ee97b0ca730819b5
srikaanthtb <srikaanth.t.b@gmail.com>
2020-10-06 12:45:59 +0100

    added pagination

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -26,5 +26,6 @@ def index(request):
     return render(request, 'index.html', context=context)
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 10
 class BookDetailView(generic.DetailView):
     model = Book
